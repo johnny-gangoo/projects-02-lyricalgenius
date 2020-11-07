cacd1babe4ecafeedd55c979dc02a47a725950a5
Johnny Gangoo <tuh34169@temple.edu>
2020-11-07 09:50:51 -0500

    fn sends back song object

--- lyricalgenius/Node_Backend/genius_api/getSongLyrics.js
+++ lyricalgenius/Node_Backend/genius_api/getSongLyrics.js
@@ -5,15 +5,14 @@ module.exports = async function (songData) {
 
     const options = {
         apiKey: 'GaXTYV3DtHSYpUqMrNjOO7sgHm-dFUmYRLWY2Pg4UPhD4gvYLkXS28EoAV0SUeje',
-        title: `${songData.SongName}`,
-        artist: `${songData.artistName}`,
+        title: songData.SongName,
+        artist: songData.artistName,
         optimizeQuery: true
     };
 
-    var data;
-    await genius.getLyrics(options).then((lyrics) => data = lyrics);
-    //this will log the all the lyrics into one big string
-    //if you want the length you can use data.length
-    console.log(data.length);
+    var song = await genius.getSong(options).then((song) => song=song)
+    //refer to https://github.com/farshed/genius-lyrics-api 
+    //which has the different options you can use within this song object
+    return song;
 
 }
