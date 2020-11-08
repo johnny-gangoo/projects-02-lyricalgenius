a68ba87e6b98722bd5e8b728663a1ef0c3d646ef
Johnny Gangoo <tuh34169@temple.edu>
2020-11-07 19:42:15 -0500

    updated the server endpoints

--- lyricalgenius/Node_Backend/server.js
+++ lyricalgenius/Node_Backend/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 const bodyParser = require("body-parser");
-var genius = require("./Genius_api/getSongLyrics");
+var genius = require("./genius_api/getSongData");
 const port = 3001;
 
 app.use(bodyParser.urlencoded({
@@ -15,12 +15,16 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-  
-app.post('/getLyrics', async (request, response) =>{
+
+app.post('/getSong', async (request, response) =>{
   let data = await genius(request.body);
   //send the lyrics (in string format) 
   //to the frontend for the user to view
-  response.send(data);
+   response.send(data);
+});
+
+app.post('/getLyrics', async (request, response) =>{
+   //**************************************//
 });
 
 app.listen(port, () => console.log("Hello from the backend server"));
