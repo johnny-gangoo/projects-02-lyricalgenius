6ab64fa44311562e727b42fd6a72f38cf8ac75cd
Johnny Gangoo <tuh34169@temple.edu>
2020-11-10 09:13:12 -0500

    UI shows handles user logic and shows basic selection panel

--- lyricalgenius/src/components/userSongInput/getUserSongInput.js
+++ lyricalgenius/src/components/userSongInput/getUserSongInput.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import './getUserSongInput.css';
 
 class GetUserInput extends Component {
     constructor(props){
@@ -7,7 +8,8 @@ class GetUserInput extends Component {
         this.state = {
             SongName: "",
             artistName: "",
-            songData: ""
+            songData: [],
+            optionsFlag: false
         }
         this.initialState=this.state;
     }
@@ -16,17 +18,19 @@ class GetUserInput extends Component {
         //this will prevent data from refreshing
         event.preventDefault();
         //axios post will send the data to the backend in JSON format
-
+        console.log(event);
         axios.post("http://localhost:3001/getSong", this.state).then(res => {
             //need to show response to user
             //this.setState({songData: res});
+
+            //need to see if i can just set songData:res and not res[0] by checking length of res upon response
             res = res.data;
             if(res == null){
                 this.setState = this.initialState;
             }else if(res.length == 1){
-                this.setState({songData:res[0]});
+                this.setState({songData:res[0], optionsFlag: false});
             }else{
-                this.setState({songData:res});
+                this.setState({songData:res, optionsFlag: true});
             }
         })
         .catch(error => {
@@ -45,9 +49,25 @@ class GetUserInput extends Component {
         });
     }
 
+    handleListItemOnClick = (d) => {
+
+        console.log(d)
+        axios.post("http://localhost:3001/getLyrics", d).then(res => {
+            //need to show response to user
+            //this.setState({songData: res});
+            //need to see if i can just set songData:res and not res[0] by checking length of res upon response
+            res = res.data;
+            this.setState({songData:[]});
+            this.setState({songData:res, optionsFlag: false});
+        })
+        .catch(error => {
+            console.log(error.response)
+        });
+        //var remove = [...this.state.songData];
+        //remove.splice(d,1)
+    }
+
     render () {
-        //below is the same as saying SongName = this.state.SongName
-        //and artistName = this.state.artistName
         const {SongName, artistName, songData} = this.state
         return (
             <div>
@@ -59,7 +79,17 @@ class GetUserInput extends Component {
                     <p><input type='text' placeholder='Enter the Artist name' name='artistName' onChange={this.handleInputChange}/></p>
                     <p><button>Submit</button></p>
                 </form>
+                {this.state.optionsFlag ? (
+                this.state.songData.map((d,index)=> (
+                    <li onClick={this.handleListItemOnClick.bind(this, d)} key={index}>
+                        <div>
+                            <p>{d.title}  {d.name}</p>
+                        </div>
+                    </li>
+                ))
+                ) : (
                 <img src={songData.albumArt}></img>
+                )}
             </div>
         )
     }
