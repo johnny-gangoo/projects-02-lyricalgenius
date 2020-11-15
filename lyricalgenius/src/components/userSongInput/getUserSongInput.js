cb7b62c69e4123fe958ab2726bcc789cf452d838
Johnny Gangoo <tuh34169@temple.edu>
2020-11-15 15:19:36 -0500

    Added preview audio tag

--- lyricalgenius/src/components/userSongInput/getUserSongInput.js
+++ lyricalgenius/src/components/userSongInput/getUserSongInput.js
@@ -1,95 +1,130 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import './getUserSongInput.css';
 
 class GetUserInput extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            SongName: "",
-            artistName: "",
+            title: "",
+            name: "",
             songData: [],
-            optionsFlag: false
+            optionsFlag: false,
+            preview: []
         }
-        this.initialState=this.state;
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.initialState = this.state;
     }
 
-    handleSubmit = (event) => {
-        //this will prevent data from refreshing
-        event.preventDefault();
-        //axios post will send the data to the backend in JSON format
-        console.log(event);
-        axios.post("http://localhost:3001/getSong", this.state).then(res => {
-            //need to show response to user
-            //this.setState({songData: res});
 
-            //need to see if i can just set songData:res and not res[0] by checking length of res upon response
-            res = res.data;
-            if(res == null){
-                this.setState = this.initialState;
-            }else if(res.length == 1){
-                this.setState({songData:res[0], optionsFlag: false});
-            }else{
-                this.setState({songData:res, optionsFlag: true});
-            }
-        })
-        .catch(error => {
-            console.log(error.response)
-        });
+    handleSubmit = (event) => {
+        if (this.state.title !== "" || this.state.name !== "") {
+            event.preventDefault();
+            axios.post("http://localhost:3001/getSong", this.state).then(res => {
+                res = res.data;
+                console.log(res);
+                if (res.length==0) {
+                    this.setState = this.initialState;
+                    alert("The song entered was not found");
+                    window.location.reload(false);
+                } else if (res.length === 1) {
+                    this.setState({ songData: res[0], optionsFlag: false });
+                } else {
+                    this.setState({ songData: res, optionsFlag: true });
+                }
+
+                this.handleSongPreview(this.state.songData)
+            })
+                .catch(error => {
+                    console.log(error.response)
+                });
+        }
     }
 
     handleInputChange = (event) => {
         event.preventDefault();
         this.setState({
-            //you can specify multiple inputs here
-            //they just need to have different names 
-            //brackets are used to dynamically update
-            //object key name
-           [event.target.name] : event.target.value
+            [event.target.name]: event.target.value
         });
     }
 
-    handleListItemOnClick = (d) => {
-
-        console.log(d)
-        axios.post("http://localhost:3001/getLyrics", d).then(res => {
-            //need to show response to user
-            //this.setState({songData: res});
-            //need to see if i can just set songData:res and not res[0] by checking length of res upon response
+    handleListItemOnClick = (songObj) => {
+        axios.post("http://localhost:3001/getLyrics", songObj).then(res => {
             res = res.data;
-            this.setState({songData:[]});
-            this.setState({songData:res, optionsFlag: false});
+            this.setState({ songData: [] });
+            this.setState({ songData: res, optionsFlag: false });
         })
-        .catch(error => {
-            console.log(error.response)
-        });
-        //var remove = [...this.state.songData];
-        //remove.splice(d,1)
+            .catch(error => {
+                console.log(error.response)
+            });
     }
 
-    render () {
-        const {SongName, artistName, songData} = this.state
+    handleSongPreview = (songObj) => {
+        if (this.state.preview.length !== 0) {
+            this.setState({ preview: [] });
+        }
+
+        songObj.map(async (songObj, index) => (
+            await axios.post("http://localhost:3001/getPreview", songObj).then(res => {
+                let new_state = Object.assign({}, this.state);
+                let a = new_state.preview;
+                a[index] = res.data;
+                this.setState({ preview: a });
+            })
+                .catch(error => {
+                    console.log(error.response)
+                })
+        ))
+    }
+
+    returnSongPreview = (prev) => {
+        var n = prev;
+        return n;
+    }
+
+    pauseOtherAudioPreviews = (event) => {
+        for(const audio of document.querySelectorAll('audio')){
+            if(audio !== event.currentTarget){
+                audio.pause();
+            }
+        }
+    }
+
+    render() {
+        const { title, name, songData } = this.state
+
         return (
             <div>
                 <h1>Input Form</h1>
-                <p>The song name is: {SongName}</p>
-                <p>The artist name is: {artistName}</p>
-                <form onSubmit={this.handleSubmit}>
-                    <p><input type='text' placeholder='Enter a Song Name' name='SongName' onChange={this.handleInputChange}/></p>
-                    <p><input type='text' placeholder='Enter the Artist name' name='artistName' onChange={this.handleInputChange}/></p>
+                <p>The song name is: {title}</p>
+                <p>The artist name is: {name}</p>
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <p><input type='text' placeholder='Enter a Song Name' name='title' onChange={this.handleInputChange.bind(this)} /></p>
+                    <p><input type='text' placeholder='Enter the Artist name' name='name' onChange={this.handleInputChange.bind(this)} /></p>
                     <p><button>Submit</button></p>
                 </form>
                 {this.state.optionsFlag ? (
-                this.state.songData.map((d,index)=> (
-                    <li onClick={this.handleListItemOnClick.bind(this, d)} key={index}>
-                        <div>
-                            <p>{d.title}  {d.name}</p>
-                        </div>
-                    </li>
-                ))
+                    this.state.songData.map((songObj, index) => (
+                        <li onClick={this.handleListItemOnClick.bind(this, songObj)} key={index}>
+                            <div>
+                                <p>{songObj.title}  {songObj.name}</p>
+                                {this.state.preview[index] === "" && 
+                                    <h1>test</h1>
+                                } 
+
+                                {this.state.preview[index] !== "" && 
+                                    <audio src={this.returnSongPreview(this.state.preview[index])} controls="controls"
+                                    onPlay={this.pauseOtherAudioPreviews} type='audio/mpeg'></audio>
+                                }
+                        
+                            </div>
+                        </li>
+                    ))
                 ) : (
-                <img src={songData.albumArt}></img>
-                )}
+                        <div>
+                            <img src={songData.albumArt}></img>
+                        </div>
+                    )}
             </div>
         )
     }
