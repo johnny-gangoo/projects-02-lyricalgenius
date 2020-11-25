aed460a1b3539fa4fc0da42d7a9c78eba9f07db7
Johnny Gangoo <tuh34169@temple.edu>
2020-11-24 21:45:11 -0500

    update displaylyricsComp

--- lyricalgenius/src/components/userSongInput/displayLyricsComponent.js
+++ lyricalgenius/src/components/userSongInput/displayLyricsComponent.js
@@ -28,16 +28,8 @@ class DisplayLyrics extends Component {
         console.log(allSections)
     }
 
-    changeColor = (songObj) => {
-            console.log($(songObj).css("background-color"))
-            if ($(songObj).css("background-color") === "rgb(51, 51, 51)") {
-                console.log("hello")
-                $(songObj).css("background-color", "#1796cf");
-            } else {
-
-                $(songObj).css("background-color", "#333333");
-            }
-        
+    changeColor = () => {
+            console.log("hi");
 
     }
 
@@ -53,7 +45,9 @@ class DisplayLyrics extends Component {
                     <li class="ListItemLyrics" onClick={() => {
                         if ($("li.ListItemLyrics").eq(index).css("background-color") === "rgb(51, 51, 51)") {
                         $("li.ListItemLyrics").eq(index).css("background-color", "#1796cf");
+                        this.changeColor();
                     } else {
+                        this.changeColor();
                         $("li.ListItemLyrics").eq(index).css("background-color", "#EBF2FF");
                     }}}   
                     >
