6e522c1771b9fb07bdab4a286f93096c71977a2f
Johnny Gangoo <tuh34169@temple.edu>
2020-11-28 11:51:07 -0500

    Added toast import

--- lyricalgenius/src/App.js
+++ lyricalgenius/src/App.js
@@ -1,16 +1,20 @@
 //import './App.css';
 import Signin from './components/pages/signin.js';
 import GetUserInput from './components/userSongInput/getUserSongInput.js';
-import Cards from './components/userSongInput/Cards/Cards.jsx';
+import '../node_modules/jquery/dist/jquery.min.js'
+import {toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/userSongInput/navbar';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/bootstrap/js/dist/dropdown.js';
+import '../node_modules/popper.js'
 import './login.css';
 import React, {Component} from 'react';
 import Error from './components/pages/404.js';
 
-
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
 
+toast.configure()
 class App extends Component{
   render(){
     return(
