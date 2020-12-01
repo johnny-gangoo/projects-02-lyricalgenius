72afbb97b761409765a1502aad11376579300a00
Kyle Skelly <kyle.skelly32@gmail.com>
2020-11-30 22:37:05 -0500

    Touched up front end

--- lyricalgenius/src/components/pages/aboutus.js
+++ lyricalgenius/src/components/pages/aboutus.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './aboutus.css';
-import githublogo from '../../images/icons/githubicon.png';
+import githublogo from '../../images/icons/whitegithub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fa } from '@fortawesome/free-solid-svg-icons'
 
 class AboutUs extends Component {
     render() {
@@ -8,57 +10,27 @@ class AboutUs extends Component {
             <div>
                 <div class="py-5 darkbackground">
                     <div class="container py-5 rounded textcontainer">
-                        <h1 class="display-4">Behind LyricalGenius</h1>
-                        <p class="lead">
+                        <h1 class="display-4 aboutuslargetext">Behind LyricalGenius</h1>
+                        <p class="lead aboutussmalltext">
                             LyricalGenius was started by 4 Temple University students as a class project. We wanted to create a quick and easy
                             way to search for and send lyrics to friends and family. At the time of development, COVID-19 was a huge issue and 
                             we felt that not only was it important to be able to reach out to loved ones, lyrics often put into words what we struggled
                             to convey. Thus, LyricalGenius was born.
                         </p>
-                        
-                        {/* <div class="row text-center">
-                            <div class="col-xl-4 col-sm-4 mb-5">
-                                <div class="bg-white rounded shadow-sm py-5 px-4">
-                                    <h5 class="mb-0">Simple</h5>
-                                    <span class="small text-uppercase text-muted">CEO - Founder</span>
-                                    <ul class="social mb-0 list-inline mt-3">
-                                        <img class="icon" src={githublogo} href />
-                                    </ul>
-                                </div>
-                            </div>
-
-                            <div class="col-xl-4 col-sm-4 mb-5">
-                                <div class="bg-white rounded shadow-sm py-5 px-4">
-                                    <h5 class="mb-0">Powerful</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
-                                    <ul class="social mb-0 list-inline mt-3">
-                                        <img class="icon" src={githublogo} href />
-                                    </ul>
-                                </div>
-                            </div>
-
-                            <div class="col-xl-4 col-sm-4 mb-5">
-                                <div class="bg-white rounded shadow-sm py-5 px-4">
-                                    <h5 class="mb-0">Intuitive</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
-                                    <ul class="social mb-0 list-inline mt-3">
-                                        <img class="icon" src={githublogo} href />
-                                    </ul>
-                                </div>
-                            </div>
-                        </div> */}
                     </div>
                     <div class="container py-5 rounded textcontainer">
                         {/*  */}
                         <div class="row mb-4">
                             <div class="col-lg-9">
-                                <h2 class="display-4 font-weight-light">Meet the Team</h2>
-                                <p class="font-italic lead ">These are the developers that have made LyricalGenius possible.</p>
+                                <h2 class="display-4 font-weight-light aboutuslargetext">Meet the Team</h2>
+                                <p class="font-italic lead aboutussmalltext">These are the developers that have made LyricalGenius possible.</p>
                             </div>
                         </div>
 
                         <div class="row text-center">
                             <div class="col-xl-3 col-sm-6 mb-5">
-                                <div class="bg-white rounded shadow-sm py-5 px-4">
-                                    <h5 class="mb-0">Jason Xhoxhi</h5><span class="small text-uppercase text-muted">Developer</span>
+                                <div class="rounded py-5 px-4">
+                                    <h5 class="mb-0 aboutuslargetext">Jason Xhoxhi</h5><span class="small text-uppercase text-muted job">Developer</span>
                                     <ul class="social mb-0 list-inline mt-3">
                                     <a href="https://github.com/JasonXhoxhi" title=""><img src={githublogo} class="icon" /></a>
                                     </ul>
@@ -66,8 +38,8 @@ class AboutUs extends Component {
                             </div>
 
                             <div class="col-xl-3 col-sm-6 mb-5">
-                                <div class="bg-white rounded shadow-sm py-5 px-4">
-                                    <h5 class="mb-0">Kyle Skelly</h5><span class="small text-uppercase text-muted">Developer</span>
+                                <div class="rounded py-5 px-4">
+                                    <h5 class="mb-0 aboutuslargetext">Kyle Skelly</h5><span class="small text-uppercase text-muted job">Developer</span>
                                     <ul class="social mb-0 list-inline mt-3">
                                         <a href="https://github.com/Kyle-Skelly" title=""><img src={githublogo} class="icon" /></a>
                                     </ul>
@@ -75,8 +47,8 @@ class AboutUs extends Component {
                             </div>
 
                             <div class="col-xl-3 col-sm-6 mb-5">
-                                <div class="bg-white rounded shadow-sm py-5 px-4">
-                                    <h5 class="mb-0">Johnny Gangoo</h5><span class="small text-uppercase text-muted">Developer</span>
+                                <div class="rounded py-5 px-4">
+                                    <h5 class="mb-0 aboutuslargetext">Johnny Gangoo</h5><span class="small text-uppercase text-muted job">Developer</span>
                                     <ul class="social mb-0 list-inline mt-3">
                                     <a href="https://github.com/johnnygangoo" title=""><img src={githublogo} class="icon" /></a>
                                     </ul>
@@ -84,8 +56,8 @@ class AboutUs extends Component {
                             </div>
 
                             <div class="col-xl-3 col-sm-6 mb-5">
-                                <div class="bg-white rounded shadow-sm py-5 px-4">
-                                    <h5 class="mb-0">Vlad K</h5><span class="small text-uppercase text-muted">Developer</span>
+                                <div class="rounded py-5 px-4">
+                                    <h5 class="mb-0 aboutuslargetext">Vlad K</h5><span class="small text-uppercase text-muted job">Developer</span>
                                     <ul class="social mb-0 list-inline mt-3">
                                     <a href="https://github.com/NotASpamBot" title=""><img src={githublogo} class="icon" /></a>
                                     </ul>
